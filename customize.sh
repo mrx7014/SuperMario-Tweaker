ad8583cc72b115dd03e209ef431f8cd207655174
MRX7014 | Octopus <safen894@gmail.com>
2025-05-05 22:08:39 +0300

    FIX

--- customize.sh
+++ customize.sh
@@ -40,7 +40,7 @@ sleep 0.5
 ui_print "- [14/14] Applying final optimizations for peak system performance & responsiveness... ⚙️"
 sleep 0.5
 ui_print "Now Starting SuperMario-Tweaker Script"
-"${MODDIR}/SuperMario-Tweaker.sh" > /dev/null
+${MODDIR}/SuperMario-Tweaker.sh" > /dev/null
 set_perm_recursive $MODPATH/system 0 0 755 u:object_r:system_file:s0
 set_perm_recursive $MODPATH/system/vendor 0 2000 755 u:object_r:vendor_file:s0
 set_perm $MODPATH/system/vendor/lib64/hw/vulkan.adreno.so 0 0 0644 u:object_r:same_process_hal_file:s0
