96139c2d1c6f42ccf5953a2eb814be3a8af4bea7
MRX7014 | Octopus <safen894@gmail.com>
2025-06-21 00:28:38 +0300

    v3.0.1

--- customize.sh
+++ customize.sh
@@ -12,14 +12,11 @@ sleep 2
 ui_print ""
 ui_print "Now Starting SuperMario-Tweaker Script"
 sh "${MODPATH}/SuperMario-Tweaker.sh" > /dev/null
-set_perm_recursive $MODPATH/system 0 0 755 u:object_r:system_file:s0
-set_perm_recursive $MODPATH/system/vendor 0 2000 755 u:object_r:vendor_file:s0
-set_perm $MODPATH/system/vendor/lib64/hw/vulkan.adreno.so 0 0 0644 u:object_r:same_process_hal_file:s0
 ui_print ""
 sleep 2
 ui_print "Done"
 ui_print ""
 ui_print "✅ Tweaks Applied Successfully!"
-ui_print "〽️ SuperMario-Tweaker v3.0.0 - MTK"
+ui_print "〽️ SuperMario-Tweaker v3.0.1 - MTK"
 ui_print "📢 Join Telegram: @MRX7014Cloud"
 ui_print "💡 Reboot to activate changes"
